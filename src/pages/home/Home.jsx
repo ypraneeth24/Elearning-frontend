d4bb858c2d31d9288dc048b57d689188ec1e7d56
ypraneeth24 <yeshaboinapraneeth@gmail.com>
2024-12-29 18:46:36 +0530

    Update Home.jsx

--- src/pages/home/Home.jsx
+++ src/pages/home/Home.jsx
@@ -1,7 +1,7 @@
 import React from "react";
 import {useNavigate} from "react-router-dom";
 import "./home.css";
-import Testimonials from "../../components/testimonials/testimonials";
+import Testimonials from "../../components/testimonials/Testimonials";
 
 const Home = () => {
   const navigate = useNavigate();
