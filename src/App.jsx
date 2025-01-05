b0e519d67f7c8b5162016cae73e168c8f969c496
ypraneeth24 <yeshaboinapraneeth@gmail.com>
2025-01-05 18:23:32 +0530

    small typo error in home and app js

--- src/App.jsx
+++ src/App.jsx
@@ -2,7 +2,7 @@ import React from "react";
 import "./App.css";
 import { BrowserRouter, Routes, Route } from "react-router-dom";
 import Home from "./pages/home/Home";
-import Header from "./components/header/header";
+import Header from "./components/header/Header";
 import Login from "./pages/auth/Login";
 import Register from "./pages/auth/Register";
 import Verify from "./pages/auth/Verify";
